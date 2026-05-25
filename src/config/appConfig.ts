6dd48898a6f21b9a3696794d51e00ee984efe0cc
ray.yau <a05166ray@gmail.com>
2026-05-25 23:07:53 +0800

    k3s-deployment fix path

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -2,5 +2,5 @@ export const APP_CONFIG = {
     SB_API_URL: "/api",
     MCP_CLIENT_PATH: "/mcp-chat",
     PAYPAL_WEBHOOK: "/webhook/paypal/status",
-    PYMT_NON_PAID_ORDER: "/non-paid-orders"
+    PYMT_NON_PAID_ORDER: "/order/non-paid-orders"
 };
